9f80afaaf3dd21ddaf7c4cb19b982383ac63e4e5
MarEscVer <maria.escribanoverde@gmail.com>
2022-11-17 12:07:38 +0100

    FINAL

---
+++ 06/src/unico/Ejercicio06.java
@@ -0,0 +1,35 @@
+package unico;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+
+// Implementar un método que nos diga si un número es primo.
+
+	final String PRIMO = "Es un número primo.";
+	final String NOPRIMO = "No es un número primo.";
+	int num;
+	boolean resultado;
+	String mensaje;
+	
+	num = Util.leerInt("Introduzca un número: ");
+	resultado = esPrimo (num);
+	
+	mensaje = resultado ? PRIMO : NOPRIMO;
+	Util.escribir(mensaje);
+		
+	}
+	
+	static public boolean esPrimo (int n) {
+
+		boolean esPrimo = true;
+		
+		for (int i = 2; 2 <n; i++) {
+			if (n%i == 0) {
+				esPrimo = false;
+			}
+		}	
+		return esPrimo;
+	}
+	
+}
